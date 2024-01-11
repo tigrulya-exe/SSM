d7e906ea3e88dd04b9b3cf9ac5bbdc9abd168483
Tigran Manasyan <t.manasyan@arenadata.io>
2024-01-11 14:08:18 +0400

    [ADH-3953] add tests and documentation in the actions help page

--- smart-hadoop-support/smart-hadoop/src/main/java/org/smartdata/hdfs/action/DistCpAction.java
+++ smart-hadoop-support/smart-hadoop/src/main/java/org/smartdata/hdfs/action/DistCpAction.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.smartdata.hdfs.action;
 
 import com.google.common.collect.Sets;
@@ -7,10 +25,12 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import org.apache.hadoop.mapreduce.Cluster;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.tools.DistCp;
 import org.apache.hadoop.tools.DistCpOptions;
 import org.apache.hadoop.tools.OptionsParser;
+import org.smartdata.action.Utils;
 import org.smartdata.action.annotation.ActionSignature;
 
 @ActionSignature(
@@ -18,70 +38,96 @@ import org.smartdata.action.annotation.ActionSignature;
     displayName = "DistCp",
     usage = DistCpAction.FILE_PATH + " $path "
         + DistCpAction.TARGET_ARG + " $target "
-        + " [additional options from " +
-        "https://hadoop.apache.org/docs/stable/hadoop-distcp/DistCp.html#Command_Line_Options]"
+        + " [additional options from "
+        + "https://hadoop.apache.org/docs/stable/hadoop-distcp/DistCp.html#Command_Line_Options]"
 )
 public class DistCpAction extends HdfsAction {
 
-    public static final String SOURCE_PATHS_DELIMITER = ",";
-    public static final String TARGET_ARG = "-target";
+  public static final String TARGET_ARG = "-target";
+  public static final String SOURCE_PATH_LIST_FILE = "-f";
 
-    public static final String SOURCE_PATH_LIST_FILE = "-f";
+  private static final String SOURCE_PATHS_DELIMITER = ",";
+  private static final Set<String> NON_DISTCP_OPTIONS = Sets.newHashSet(FILE_PATH, TARGET_ARG);
 
-    public static final Set<String> NON_DISTCP_OPTIONS = Sets.newHashSet(FILE_PATH, TARGET_ARG);
+  private DistCpOptions options;
 
-    private DistCpOptions options;
+  @Override
+  public void init(Map<String, String> args) {
+    super.init(args);
+    validateRequiredOptions(args);
 
-    @Override
-    public void init(Map<String, String> args) {
-        super.init(args);
-        validateRequiredOptions(args);
+    List<String> rawArgs = args.entrySet()
+        .stream()
+        .filter(entry -> !NON_DISTCP_OPTIONS.contains(entry.getKey()))
+        .flatMap(entry -> mapOptionToStr(entry.getKey(), entry.getValue()))
+        .collect(Collectors.toList());
 
-        List<String> rawArgs = args.entrySet()
-            .stream()
-            .filter(entry -> !NON_DISTCP_OPTIONS.contains(entry.getKey()))
-            .flatMap(entry -> mapOptionToStr(entry.getKey(), entry.getValue()))
-            .collect(Collectors.toList());
+    if (!args.containsKey(SOURCE_PATH_LIST_FILE)) {
+      rawArgs.addAll(parseSourcePaths(args.get(FILE_PATH)));
+    }
+    rawArgs.add(args.get(TARGET_ARG));
 
-        if (!args.containsKey(SOURCE_PATH_LIST_FILE)) {
-            rawArgs.addAll(parseSourcePaths(args.get(FILE_PATH)));
-        }
-        rawArgs.add(args.get(TARGET_ARG));
+    options = OptionsParser.parse(rawArgs.toArray(new String[0]));
+  }
 
-        options = OptionsParser.parse(rawArgs.toArray(new String[0]));
+  @Override
+  protected void execute() throws Exception {
+    DistCp distCp = new DistCp(getContext().getConf(), options);
+    appendLog(
+        String.format("DistCp Action started at %s for options %s",
+            Utils.getFormatedCurrentTime(), options));
+
+    try (JobCloseableWrapper jobWrapper = new JobCloseableWrapper(distCp.execute())) {
+      distCp.waitForJobCompletion(jobWrapper.job);
+    }
+  }
+
+  DistCpOptions getOptions() {
+    return options;
+  }
+
+  private List<String> parseSourcePaths(String sourcePaths) {
+    return Arrays.asList(sourcePaths.split(SOURCE_PATHS_DELIMITER));
+  }
+
+  private void validateRequiredOptions(Map<String, String> args) {
+    if (!args.containsKey(FILE_PATH) && !args.containsKey(SOURCE_PATH_LIST_FILE)) {
+      throw new IllegalArgumentException("Source paths not provided, please provide either "
+          + FILE_PATH + " either " + SOURCE_PATH_LIST_FILE + " argument");
+    }
+
+    if (args.containsKey(FILE_PATH) && args.containsKey(SOURCE_PATH_LIST_FILE)) {
+      throw new IllegalArgumentException(FILE_PATH + " and " + SOURCE_PATH_LIST_FILE
+          + " can't be used at the same time. Use only one of the options for specifying source paths.");
+    }
+
+    if (!args.containsKey(TARGET_ARG)) {
+      throw new IllegalArgumentException("Required argument not present: " + TARGET_ARG);
+    }
+  }
+
+  private Stream<String> mapOptionToStr(String key, String value) {
+    if (value.isEmpty()) {
+      return Stream.of(key);
+    }
+    return Stream.of(key, value);
+  }
+
+  /** Used to gracefully close MapReduce job (MR Job is not AutoCloseable inheritor in Hadoop 2.7) */
+  private static class JobCloseableWrapper implements AutoCloseable {
+
+    private final Job job;
+
+    private JobCloseableWrapper(Job job) {
+      this.job = job;
     }
 
     @Override
-    protected void execute() throws Exception {
-        DistCp distCp = new DistCp(getContext().getConf(), options);
-        try (Job copyJob = distCp.execute()) {
-            distCp.waitForJobCompletion(copyJob);
-        }
+    public void close() throws Exception {
+      Cluster cluster = job.getCluster();
+      if (cluster != null) {
+        cluster.close();
+      }
     }
-
-    DistCpOptions getOptions() {
-        return options;
-    }
-
-    private List<String> parseSourcePaths(String sourcePaths) {
-        return Arrays.asList(sourcePaths.split(SOURCE_PATHS_DELIMITER));
-    }
-
-    private void validateRequiredOptions(Map<String, String> args) {
-        if (!args.containsKey(FILE_PATH) && !args.containsKey(SOURCE_PATH_LIST_FILE)) {
-            throw new IllegalArgumentException("Source paths not provided, please provide either "
-                + FILE_PATH + " either " + SOURCE_PATH_LIST_FILE + " argument");
-        }
-
-        if (!args.containsKey(TARGET_ARG)) {
-            throw new IllegalArgumentException("Required argument not present: " + TARGET_ARG);
-        }
-    }
-
-    private Stream<String> mapOptionToStr(String key, String value) {
-        if (value.isEmpty()) {
-            return Stream.of(key);
-        }
-        return Stream.of(key, value);
-    }
+  }
 }
--- smart-hadoop-support/smart-hadoop/src/test/java/org/smartdata/hdfs/action/TestDistCpAction.java
+++ smart-hadoop-support/smart-hadoop/src/test/java/org/smartdata/hdfs/action/TestDistCpAction.java
@@ -21,12 +21,10 @@ package org.smartdata.hdfs.action;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import org.apache.curator.shaded.com.google.common.collect.Sets;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
@@ -46,144 +44,202 @@ import org.smartdata.hdfs.MiniClusterHarness;
  */
 public class TestDistCpAction extends MiniClusterHarness {
 
-    @Rule
-    public TemporaryFolder tmpFolder = new TemporaryFolder();
+  @Rule
+  public TemporaryFolder tmpFolder = new TemporaryFolder();
 
-    public DistCpAction createAction(Map<String, String> args) {
-        DistCpAction distCpAction = new DistCpAction();
-        distCpAction.setDfsClient(dfsClient);
-        distCpAction.setContext(smartContext);
-        distCpAction.init(args);
-        return distCpAction;
-    }
+  public DistCpAction createAction(Map<String, String> args) {
+    DistCpAction distCpAction = new DistCpAction();
+    distCpAction.setDfsClient(dfsClient);
+    distCpAction.setContext(smartContext);
+    distCpAction.init(args);
+    return distCpAction;
+  }
 
-    @Test
-    public void testParseSingleSource() {
-        Map<String, String> args = new HashMap<>();
-        args.put(DistCpAction.FILE_PATH, "/test/source/dir1");
-        args.put(DistCpAction.TARGET_ARG, "hdfs://nn2/test/target/dir1");
-        DistCpAction action = createAction(args);
-        DistCpOptions distCpOptions = action.getOptions();
+  @Test
+  public void testParseSingleSource() {
+    Map<String, String> args = new HashMap<>();
+    args.put(DistCpAction.FILE_PATH, "/test/source/dir1");
+    args.put(DistCpAction.TARGET_ARG, "hdfs://nn2/test/target/dir1");
+    DistCpAction action = createAction(args);
+    DistCpOptions distCpOptions = action.getOptions();
 
-        Path expectedSource = new Path("/test/source/dir1");
-        Assert.assertEquals(Collections.singletonList(expectedSource),
-            distCpOptions.getSourcePaths());
-        Assert.assertEquals(new Path("hdfs://nn2/test/target/dir1"),
-            distCpOptions.getTargetPath());
-    }
+    Path expectedSource = new Path("/test/source/dir1");
+    Assert.assertEquals(Collections.singletonList(expectedSource),
+        distCpOptions.getSourcePaths());
+    Assert.assertEquals(new Path("hdfs://nn2/test/target/dir1"),
+        distCpOptions.getTargetPath());
+  }
 
-    @Test
-    public void testParseSeveralSources() {
-        Map<String, String> args = new HashMap<>();
-        args.put(DistCpAction.FILE_PATH, "/test/source/dir1,/test/source/dir2,/test/source/dir3");
-        args.put(DistCpAction.TARGET_ARG, "hdfs://nn2/test/target/dir1");
-        DistCpAction action = createAction(args);
-        DistCpOptions distCpOptions = action.getOptions();
+  @Test
+  public void testParseSeveralSources() {
+    Map<String, String> args = new HashMap<>();
+    args.put(DistCpAction.FILE_PATH, "/test/source/dir1,/test/source/dir2,/test/source/dir3");
+    args.put(DistCpAction.TARGET_ARG, "hdfs://nn2/test/target/dir1");
+    DistCpAction action = createAction(args);
+    DistCpOptions distCpOptions = action.getOptions();
 
-        List<Path> expectedSources = Stream.of(
+    List<Path> expectedSources = Stream.of(
             "/test/source/dir1", "/test/source/dir2", "/test/source/dir3")
-            .map(Path::new)
-            .collect(Collectors.toList());
-        Assert.assertEquals(expectedSources,
-            distCpOptions.getSourcePaths());
-        Assert.assertEquals(new Path("hdfs://nn2/test/target/dir1"),
-            distCpOptions.getTargetPath());
+        .map(Path::new)
+        .collect(Collectors.toList());
+    Assert.assertEquals(expectedSources,
+        distCpOptions.getSourcePaths());
+    Assert.assertEquals(new Path("hdfs://nn2/test/target/dir1"),
+        distCpOptions.getTargetPath());
+  }
+
+  @Test
+  public void testParseSourceFileListingOption() {
+    final String sourcesPath = "/test/sources.txt";
+
+    Map<String, String> args = new HashMap<>();
+    args.put(DistCpAction.SOURCE_PATH_LIST_FILE, sourcesPath);
+    args.put(DistCpAction.TARGET_ARG, "hdfs://nn2/test/target/dir1");
+    DistCpAction action = createAction(args);
+    DistCpOptions distCpOptions = action.getOptions();
+
+    Assert.assertEquals(new Path(sourcesPath),
+        distCpOptions.getSourceFileListing());
+    Assert.assertEquals(new Path("hdfs://nn2/test/target/dir1"),
+        distCpOptions.getTargetPath());
+  }
+
+  @Test
+  public void testThrowIfNoSourceOptionProvided() {
+    Map<String, String> args = new HashMap<>();
+    args.put(DistCpAction.TARGET_ARG, "hdfs://nn2/test/target/dir1");
+
+    IllegalArgumentException exception = Assert.assertThrows(
+        IllegalArgumentException.class, () -> createAction(args));
+    Assert.assertEquals(exception.getMessage(),
+        "Source paths not provided, please provide either -file either -f argument");
+  }
+
+  @Test
+  public void testThrowIfBothSourceOptionsProvided() {
+    Map<String, String> args = new HashMap<>();
+    args.put(DistCpAction.FILE_PATH, "test_path");
+    args.put(DistCpAction.SOURCE_PATH_LIST_FILE, "test_path_listing");
+    args.put(DistCpAction.TARGET_ARG, "hdfs://nn2/test/target/dir1");
+
+    IllegalArgumentException exception = Assert.assertThrows(
+        IllegalArgumentException.class, () -> createAction(args));
+    Assert.assertEquals(exception.getMessage(),
+        "-file and -f can't be used at the same time. Use only one of the options for specifying source paths.");
+  }
+
+  @Test
+  public void testThrowIfTargetOptionNotProvided() {
+    Map<String, String> args = new HashMap<>();
+    args.put(DistCpAction.SOURCE_PATH_LIST_FILE, "test_path_listing");
+
+    IllegalArgumentException exception = Assert.assertThrows(
+        IllegalArgumentException.class, () -> createAction(args));
+    Assert.assertEquals(exception.getMessage(), "Required argument not present: -target");
+  }
+
+  @Test
+  public void testParseDistCpOptionalArgs() {
+    Map<String, String> args = new HashMap<>();
+    args.put(DistCpAction.FILE_PATH, "/test/source/dir1");
+    args.put(DistCpAction.TARGET_ARG, "hdfs://nn2/test/target/dir1");
+    args.put("-pcat", "");
+    args.put("-m", "16");
+    args.put("-strategy", "dynamic");
+    args.put("-update", "");
+    DistCpAction action = createAction(args);
+    DistCpOptions distCpOptions = action.getOptions();
+
+    Path expectedSource = new Path("/test/source/dir1");
+
+    Assert.assertEquals(Collections.singletonList(expectedSource),
+        distCpOptions.getSourcePaths());
+    Assert.assertEquals(new Path("hdfs://nn2/test/target/dir1"),
+        distCpOptions.getTargetPath());
+    Assert.assertEquals(16, distCpOptions.getMaxMaps());
+    Assert.assertEquals("dynamic", distCpOptions.getCopyStrategy());
+    Assert.assertTrue(distCpOptions.shouldSyncFolder());
+  }
+
+  @Test
+  public void testIntraClusterCopy() throws Exception {
+    testCopyToCluster(dfs, dfs);
+  }
+
+  @Test
+  public void testCopyToAnotherCluster() throws Exception {
+    // MiniDFSCluster from hadoop 2.7 doesn't implement AutoCloseable
+    MiniDFSCluster anotherCluster = null;
+    try {
+      anotherCluster = createAnotherCluster();
+      anotherCluster.waitActive();
+      FileSystem anotherFs = anotherCluster.getFileSystem();
+      testCopyToCluster(dfs, anotherFs);
+    } finally {
+      if (anotherCluster != null) {
+        anotherCluster.shutdown();
+      }
     }
+  }
 
-    @Test
-    public void testParseDistCpOptionalArgs() {
-        Map<String, String> args = new HashMap<>();
-        args.put(DistCpAction.FILE_PATH, "/test/source/dir1");
-        args.put(DistCpAction.TARGET_ARG, "hdfs://nn2/test/target/dir1");
-        args.put("-pcat", "");
-        args.put("-m", "16");
-        args.put("-strategy", "dynamic");
-        args.put("-update", "");
-        DistCpAction action = createAction(args);
-        DistCpOptions distCpOptions = action.getOptions();
-
-        Path expectedSource = new Path("/test/source/dir1");
-        HashSet<DistCpOptions.FileAttribute> expectedPreserveAttributes = Sets.newHashSet(
-            DistCpOptions.FileAttribute.CHECKSUMTYPE,
-            DistCpOptions.FileAttribute.ACL,
-            DistCpOptions.FileAttribute.TIMES);
-
-        Assert.assertEquals(Collections.singletonList(expectedSource),
-            distCpOptions.getSourcePaths());
-        Assert.assertEquals(new Path("hdfs://nn2/test/target/dir1"),
-            distCpOptions.getTargetPath());
-        Assert.assertEquals(expectedPreserveAttributes, distCpOptions.getPreserveAttributes());
-        Assert.assertEquals(16, distCpOptions.getMaxMaps());
-        Assert.assertEquals("dynamic", distCpOptions.getCopyStrategy());
-        Assert.assertTrue(distCpOptions.shouldSyncFolder());
+  @Test
+  public void testCopyFromAnotherCluster() throws Exception {
+    MiniDFSCluster anotherCluster = null;
+    try {
+      anotherCluster = createAnotherCluster();
+      anotherCluster.waitActive();
+      FileSystem anotherFs = anotherCluster.getFileSystem();
+      testCopyToCluster(anotherFs, dfs);
+    } finally {
+      if (anotherCluster != null) {
+        anotherCluster.shutdown();
+      }
     }
+  }
 
-    @Test
-    public void testIntraClusterCopy() throws Exception {
-        testCopyToCluster(dfs, dfs);
+  private void testCopyToCluster(FileSystem sourceFs, FileSystem targetFs) throws Exception {
+    Map<String, String> args = new HashMap<>();
+    String sourcePath = sourceFs.getUri() + "/test/source/dir1";
+    String targetPath = targetFs.getUri() + "/test/target/";
+
+    args.put(DistCpAction.FILE_PATH, sourcePath);
+    args.put(DistCpAction.TARGET_ARG, targetPath);
+    DistCpAction action = createAction(args);
+
+    writeToFile(sourceFs, new Path(sourcePath + "/testFile1"), "data-1");
+    writeToFile(sourceFs, new Path(sourcePath + "/testFile2"), "another file data");
+    writeToFile(sourceFs, new Path(sourcePath + "/inner/testFile3"), "inner data");
+
+    action.execute();
+
+    assertFileContent(targetFs, new Path(targetPath + "/dir1/testFile1"), "data-1");
+    assertFileContent(targetFs, new Path(targetPath + "/dir1/testFile2"), "another file data");
+    assertFileContent(targetFs, new Path(targetPath + "/dir1/inner/testFile3"), "inner data");
+  }
+
+  private MiniDFSCluster createAnotherCluster() throws Exception {
+    Configuration clusterConfig = new Configuration(smartContext.getConf());
+    clusterConfig.set("hdfs.minidfs.basedir", tmpFolder.newFolder().getAbsolutePath());
+    return createCluster(clusterConfig);
+  }
+
+  private void assertFileContent(
+      final FileSystem fileSystem, final Path path, final String expectedData) throws IOException {
+    Assert.assertTrue(fileSystem.exists(path));
+    Assert.assertEquals(expectedData, readFromFile(fileSystem, path));
+  }
+
+  private void writeToFile(
+      final FileSystem fileSystem, final Path path, final String data) throws IOException {
+    fileSystem.mkdirs(path.getParent());
+    try (final FSDataOutputStream outputStream = fileSystem.create(path)) {
+      outputStream.writeUTF(data);
     }
+  }
 
-    @Test
-    public void testCopyToAnotherCluster() throws Exception {
-        try (MiniDFSCluster anotherCluster = createAnotherCluster()) {
-            anotherCluster.waitActive();
-            FileSystem anotherFs = anotherCluster.getFileSystem();
-            testCopyToCluster(dfs, anotherFs);
-        }
+  private String readFromFile(final FileSystem fileSystem, final Path path) throws IOException {
+    try (final FSDataInputStream inputStream = fileSystem.open(path)) {
+      return inputStream.readUTF();
     }
-
-    @Test
-    public void testCopyFromAnotherCluster() throws Exception {
-        try (MiniDFSCluster anotherCluster = createAnotherCluster()) {
-            anotherCluster.waitActive();
-            FileSystem anotherFs = anotherCluster.getFileSystem();
-            testCopyToCluster(anotherFs, dfs);
-        }
-    }
-
-    private void testCopyToCluster(FileSystem sourceFs, FileSystem targetFs) throws Exception {
-        Map<String, String> args = new HashMap<>();
-        String sourcePath = sourceFs.getUri() + "/test/source/dir1";
-        String targetPath = targetFs.getUri() + "/test/target/";
-
-        args.put(DistCpAction.FILE_PATH, sourcePath);
-        args.put(DistCpAction.TARGET_ARG, targetPath);
-        DistCpAction action = createAction(args);
-
-        writeToFile(sourceFs, new Path(sourcePath + "/testFile1"), "data-1");
-        writeToFile(sourceFs, new Path(sourcePath + "/testFile2"), "another file data");
-        writeToFile(sourceFs, new Path(sourcePath + "/inner/testFile3"), "inner data");
-
-        action.execute();
-
-        assertFileContent(targetFs, new Path(targetPath + "/dir1/testFile1"), "data-1");
-        assertFileContent(targetFs, new Path(targetPath + "/dir1/testFile2"), "another file data");
-        assertFileContent(targetFs, new Path(targetPath + "/dir1/inner/testFile3"), "inner data");
-    }
-
-    private MiniDFSCluster createAnotherCluster() throws Exception {
-        Configuration clusterConfig = new Configuration(smartContext.getConf());
-        clusterConfig.set("hdfs.minidfs.basedir", tmpFolder.newFolder().getAbsolutePath());
-        return createCluster(clusterConfig);
-    }
-
-    private void assertFileContent(
-        final FileSystem fileSystem, final Path path, final String expectedData) throws IOException {
-        Assert.assertTrue(fileSystem.exists(path));
-        Assert.assertEquals(expectedData, readFromFile(fileSystem, path));
-    }
-
-    private void writeToFile(
-        final FileSystem fileSystem, final Path path, final String data) throws IOException {
-        fileSystem.mkdirs(path.getParent());
-        try(final FSDataOutputStream outputStream = fileSystem.create(path)) {
-            outputStream.writeUTF(data);
-        }
-    }
-
-    private String readFromFile(final FileSystem fileSystem, final Path path) throws IOException {
-        try(final FSDataInputStream inputStream = fileSystem.open(path)) {
-            return inputStream.readUTF();
-        }
-    }
+  }
 }
