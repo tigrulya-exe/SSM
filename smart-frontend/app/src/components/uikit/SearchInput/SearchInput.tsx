ca2369b3ad728dc0e4b1c4003043ebb2dfe09ec3
Kirill Fedorenko <k.fedorenko@arenadata.io>
2024-05-03 14:17:29 +0300

    ADH-4387 [UI] Add Select, MultiSelect and depedent components

---
+++ smart-frontend/app/src/components/uikit/SearchInput/SearchInput.tsx
@@ -0,0 +1,35 @@
+import React, { useRef } from 'react';
+import cn from 'classnames';
+import type { InputProps } from '@uikit/Input/Input';
+import Input from '@uikit/Input/Input';
+import { useForwardRef } from '@hooks/useForwardRef';
+import { createChangeEvent } from '@utils/handlerUtils';
+import IconButton from '@uikit/IconButton/IconButton';
+
+const SearchInput = React.forwardRef<HTMLInputElement, InputProps>(({ className, ...props }, ref) => {
+  const localRef = useRef<HTMLInputElement>(null);
+  const reference = useForwardRef(ref, localRef);
+
+  const handleIconClick = () => {
+    if (props.value && localRef.current) {
+      const event = createChangeEvent(localRef.current);
+      event.target.value = '';
+      props.onChange?.(event);
+    }
+  };
+
+  return (
+    <Input
+      {...props}
+      className={cn(className, 'search-input')}
+      ref={reference}
+      endAdornment={
+        <IconButton icon={props.value ? 'close' : 'search'} onClick={handleIconClick} size={12} variant="secondary" />
+      }
+      size="medium"
+    />
+  );
+});
+
+SearchInput.displayName = 'SearchInput';
+export default SearchInput;
---
+++ smart-frontend/app/src/components/uikit/Select/CommonSelect/CommonSelectSearchFilter/CommonSelectSearchFilter.tsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { getFilteredOptions } from './CommonSelectSearchFilter.utils';
+import type { SelectOption } from '@uikit/Select/Select.types';
+import SearchInput from '@uikit/SearchInput/SearchInput';
+
+interface CommonSelectSearchFilterProps<T> {
+  originalOptions: SelectOption<T>[];
+  setOptions: (list: SelectOption<T>[]) => void;
+  searchPlaceholder?: string;
+  className?: string;
+  onSearch?: (val: string) => void;
+}
+
+const CommonSelectSearchFilter = <T,>({
+  originalOptions,
+  setOptions,
+  searchPlaceholder,
+  className,
+  onSearch,
+}: CommonSelectSearchFilterProps<T>) => {
+  const [search, setSearch] = useState('');
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchStr = e.target.value;
+    setSearch(searchStr);
+    onSearch?.(searchStr);
+  };
+
+  useEffect(() => {
+    setOptions(getFilteredOptions(originalOptions, search));
+  }, [originalOptions, search, setOptions]);
+
+  return <SearchInput className={className} placeholder={searchPlaceholder} value={search} onChange={handleSearch} />;
+};
+
+export default CommonSelectSearchFilter;
