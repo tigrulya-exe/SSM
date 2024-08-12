3514a3f0833d08f500953194e0d880160376607c
v.paskov <v.paskov@arenadata.io>
2024-08-12 14:02:29 +0300

    feature/ADH-4849

--- smart-frontend/app/src/store/adh/actions/actionsTableSlice.ts
+++ smart-frontend/app/src/store/adh/actions/actionsTableSlice.ts
@@ -60,7 +60,7 @@ const createInitialState = (): AdhTablesTableState => ({
   requestFrequency: 0,
   sortParams: {
     sortBy: 'id',
-    sortDirection: 'asc',
+    sortDirection: 'desc',
   },
   relatedData: {
     hosts: [],
