eec8de8fe49de025a30f9691b96fc5799ccaf941
Tigran Manasyan <t.manasyan@arenadata.io>
2025-10-27 15:06:23 +0400

    [ADH-6995] working source cluster

--- build-project.sh
+++ build-project.sh
@@ -3,5 +3,5 @@ set -e
 
 HDFS_VERSION=$1
 
-mvn install:install-file -pl smart-tests
-mvn clean install -Pdist,web-ui,hadoop-"${HDFS_VERSION}" -DskipTests
+mvn install:install-file -pl smart-tests -Dos.arch=x86_64
+mvn clean install -Pdist,web-ui,hadoop-"${HDFS_VERSION}" -DskipTests -Dos.arch=x86_64
